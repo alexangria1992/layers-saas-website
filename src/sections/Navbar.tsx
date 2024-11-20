17c7382c0137a6c4026964dc4d5eafcc811c258a
Alexandre Angrignon <alex040892@gmail.com>
2024-11-20 21:28:19 +1300

    Tablet Navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import logoImage from "@/assets/images/logo.svg";
+import Button from "@/components/Button";
+
 const navLinks = [
     { label: "Home", href: "#" },
     { label: "Features", href: "#features" },
@@ -11,11 +13,15 @@ export default function Navbar() {
     return (
         <section className="py-4">
             <div className="container ">
-                <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
+                <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center md:pr-2">
                     <div>
-                        <Image src={logoImage} alt="" className="h-9 w-auto " />
+                        <Image
+                            src={logoImage}
+                            alt=""
+                            className="h-9 w-auto md:h-auto"
+                        />
                     </div>
-                    <div className="flex justify-end">
+                    <div className="flex justify-end gap-4">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
@@ -26,12 +32,14 @@ export default function Navbar() {
                             stroke-width="2"
                             stroke-linecap="round"
                             stroke-linejoin="round"
-                            className="feather feather-menu"
+                            className="feather feather-menu md:hidden"
                         >
                             <line x1="3" y1="12" x2="21" y2="12"></line>
                             <line x1="3" y1="6" x2="21" y2="6"></line>
                             <line x1="3" y1="18" x2="21" y2="18"></line>
                         </svg>
+                        <Button variant="secondary">Log In</Button>
+                        <Button variant="primary">Sign Up</Button>
                     </div>
                 </div>
             </div>
