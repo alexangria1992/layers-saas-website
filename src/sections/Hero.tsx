738981ac27c88cbf6ccc013640107cf06a9e55be
Alexandre Angrignon <alex040892@gmail.com>
2024-11-22 22:56:49 +1300

    Desktop Hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,31 @@
 import Button from "@/components/Button";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
 
 export default function Hero() {
     return (
-        <section className="py-24">
-            <div className="container">
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-60  top-16 hidden lg:block">
+                    <Image src={designExample1Image} alt="" />
+                </div>
+                <div className="absolute -right-[380px] top-16  hidden lg:block">
+                    <Image src={designExample2Image} alt="" />
+                </div>
+                <div className="absolute left-56 top-96  hidden lg:block">
+                    <Pointer name="Andrea" />
+                </div>
+                <div className="absolute right-80 -top-4  hidden lg:block ">
+                    <Pointer name="Bryan" color="red" />
+                </div>
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                         ✨ $7.5M seed round raised
                     </div>
                 </div>
-                <h1 className="text-6xl md:text-7xl font-medium text-center mt-6">
+                <h1 className="text-6xl font-medium text-center mt-6 md:text-7xl lg:text-8xl ">
                     Impactful design, crafted effortlessly
                 </h1>
                 <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
