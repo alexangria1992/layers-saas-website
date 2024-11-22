6a52cfe6efd70fe4a3f43bedc8c171c26bbef585
Alexandre Angrignon <alex040892@gmail.com>
2024-11-22 22:09:02 +1300

    Tablet Hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,22 +9,22 @@ export default function Hero() {
                         ✨ $7.5M seed round raised
                     </div>
                 </div>
-                <h1 className="text-6xl font-medium text-center mt-6">
+                <h1 className="text-6xl md:text-7xl font-medium text-center mt-6">
                     Impactful design, crafted effortlessly
                 </h1>
-                <p className="text-center text-xl text-white/50 mt-8">
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Veniam necessitatibus totam eveniet iure eum. Excepturi
                     nobis animi laborum tempore veritatis explicabo
                 </p>
                 <form
                     action=""
-                    className="flex border border-white/15 rounded-full p-2 mt-8"
+                    className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto "
                 >
                     <input
                         type="email"
                         placeholder="Enter your email"
-                        className="bg-transparent px-4"
+                        className="bg-transparent px-4 md:flex-1"
                     />
                     <Button
                         type="submit"
